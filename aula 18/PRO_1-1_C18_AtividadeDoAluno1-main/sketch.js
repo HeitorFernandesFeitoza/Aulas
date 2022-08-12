72d6c0069812dec3782bb96cdea60cae5b72e4c4
HeitorFernandesFeitoza <109107407+HeitorFernandesFeitoza@users.noreply.github.com>
2022-08-12 17:02:06 -0300

    Add files via upload

---
+++ aula 18/PRO_1-1_C18_AtividadeDoAluno1-main/sketch.js
@@ -0,0 +1,12 @@
+var student1;
+function setup() {
+  createCanvas(400,400);
+  student1 = new Student("Ricardo",16,8);
+  student1.show();
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ aula 18/PRO_1-1_C18_AtividadeDoAluno1-main/student.js
@@ -0,0 +1,18 @@
+
+class Student
+{
+    constructor(nome,idade,nota){
+        this.nome = nome;
+        this.idade = idade;
+        this.nota = nota;
+    }
+    show(){
+        console.log(this.nome);
+        console.log(this.idade);
+        console.log(this.nota);
+    }
+
+}
+
+
+
